b36dc2802b0dfee651d2866b56c69bffe2b2fd19
LisandroDelReal <lldelreal@outlook.com>
2022-04-22 11:55:37 -0300

    que mal que la estoy pasando

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
 
@@ -10,7 +10,7 @@ export default function App() {
 
   const checkIfWalletIsConnected = async () => {
 
-    
+
     /*
     * First make sure we have access to window.ethereum
     */
@@ -38,31 +38,31 @@ export default function App() {
     } catch (error) {
       console.log(error);
     }
-    useEffect(() => {
-      checkIfWalletIsConnected();
-    }, [])
-
-    const wave = () => {
-
-    }
-
-    return (
-      <div className="mainContainer">
-
-        <div className="dataContainer">
-          <div className="header">
-            👋 Hey there!
-          </div>
-
-          <div className="bio">
-            I am lisandro! Connect your Ethereum wallet and wave at me!
-          </div>
-
-          <button className="waveButton" onClick={wave}>
-            Wave at Me
-          </button>
-        </div>
-      </div>
-    );
   }
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+  const wave = () => {
+
+  }
+
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+          👋 Hey there!
+        </div>
+
+        <div className="bio">
+          I am lisandro! Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
 }
