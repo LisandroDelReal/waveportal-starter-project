33ac04921a8b9b4c6c75a8b3c43811d7e269788f
LisandroDelReal <lldelreal@outlook.com>
2022-04-22 12:02:47 -0300

    connect wallet button

--- src/App.js
+++ src/App.js
@@ -39,6 +39,29 @@ export default function App() {
       console.log(error);
     }
   }
+
+  /**
+   * ConnectWallet method
+   */
+  const connectWallet = async () => {
+    try{
+      const { ethereum } = window;
+      
+      if(!ethereum){
+        alert("Get Metamask");
+        return;
+      }
+
+      const accounts = await ethereum.request({method: "eth_requestAccounts"});
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    }catch (error) {
+      console.log(error);
+    }
+  }
+
+
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
@@ -62,6 +85,16 @@ export default function App() {
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+
+
+        {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
